1e0b0f2be694279111ecb002c44ecd448fe1d155
willclarktech <willclarktech@users.noreply.github.com>
2018-04-06 20:04:48 +0200

    :construction_worker: Update build for 1.0.0-beta.1

--- dist-node/lisk-constants/index.js
+++ dist-node/lisk-constants/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 /*
  * Copyright © 2017 Lisk Foundation
@@ -27,7 +27,7 @@ var MAX_ADDRESS_NUMBER = exports.MAX_ADDRESS_NUMBER = '18446744073709551615';
 var MAX_TRANSACTION_AMOUNT = exports.MAX_TRANSACTION_AMOUNT = '18446744073709551615';
 
 var BETANET_NETHASH = exports.BETANET_NETHASH = 'ef3844327d1fd0fc5785291806150c937797bdb34a748c9cd932b7e859e9ca0c';
-var BETANET_NODES = exports.BETANET_NODES = ['http://94.237.41.99:5000', 'http://209.50.52.217:5000', 'http://94.237.26.150:5000', 'http://83.136.249.102:5000', 'http://94.237.65.179:5000'];
+var BETANET_NODES = exports.BETANET_NODES = ['http://94.237.42.109:5000', 'http://83.136.252.99:5000'];
 var TESTNET_NETHASH = exports.TESTNET_NETHASH = 'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba';
 var TESTNET_NODES = exports.TESTNET_NODES = ['http://testnet.lisk.io:7000'];
 var MAINNET_NETHASH = exports.MAINNET_NETHASH = 'ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511';
